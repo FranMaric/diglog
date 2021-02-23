840c572e66b28254e4d294ff109d5985db748162
Fran Marić (komp-VK) <fm.develoloper@gmail.com>
2021-02-23 13:33:22 +0100

    Inital app layout

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,51 @@
 import 'package:flutter/material.dart';
 
 //Widgets
-import 'package:diglog/widgets/KTable.dart';
+import 'package:diglog/widgets/InputSelector.dart';
+import 'package:diglog/widgets/OutputPane.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: KTable(
-        variableNames: ["A", "B", "C", "D"],
-        states: [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      body: Column(
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: Text(
+                "Function minimizer",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontFamily: "Roboto",
+                ),
+              ),
+            ),
+          ),
+          Container(
+            height: 1,
+            color: Colors.black,
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Expanded(
+                child: InputSelector(),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    OutputPane(
+                      variableNames: ["A", "B", "C"],
+                      states: [0, 0, 1, 0, 1, 0, 0, 0],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }

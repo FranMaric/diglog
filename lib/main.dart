b762df105fe9d84adf4ee97bf6780fe0e746ac37
Fran Marić (komp-VK) <fm.develoloper@gmail.com>
2021-02-23 13:34:13 +0100

    Refactored main.dart to minimize it's size

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:diglog/app.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'DigLog',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+void main() => runApp(
+      MaterialApp(
+        title: 'DigLog',
+        debugShowCheckedModeBanner: false,
+        home: App(),
       ),
-      home: App(),
     );
-  }
-}

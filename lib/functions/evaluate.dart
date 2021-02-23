3964f751eacc499a38a4020aa9903c6f99da00d5
Fran Marić (komp-VK) <fm.develoloper@gmail.com>
2021-02-24 00:04:58 +0100

    Evaluator working but needs hard testing

--- lib/functions/evaluate.dart
+++ lib/functions/evaluate.dart
@@ -1,10 +1,51 @@
 // expression must not contain spaces!!!
 // expression can contain only !,+,*,0,1
 int evaluate(String expression) {
+  //Remove useless paranthesis
+  expression = expression.replaceAll("()", "");
+
+  List<int> paranthesis = [];
+  int depth = 0;
+  for (int i = 0; i < expression.length; i++) {
+    if (expression[i] == "(") {
+      depth++;
+      if (depth == 1) {
+        paranthesis.add(i);
+      }
+    } else if (expression[i] == ")") {
+      if (depth == 1) {
+        paranthesis.add(i);
+      }
+      depth--;
+    }
+  }
+  if (depth != 0) print("Paranthesis are messed up!");
+
+  //Evaluate each depth=1 paranthesis seperately = recursion - uuuuu fancy
+  for (int i = 0; i < paranthesis.length / 2; i++) {
+    if (paranthesis[i * 2] - 1 >= 0 &&
+        expression[paranthesis[i * 2] - 1] == "!") {
+      expression = expression.substring(0, paranthesis[i * 2] - 1) +
+          (evaluate(expression.substring(
+                      paranthesis[i * 2] + 1, paranthesis[i * 2 + 1])) ==
+                  0
+              ? "1"
+              : "0") +
+          expression.substring(paranthesis[i * 2 + 1] + 1);
+    } else {
+      expression = expression.substring(0, paranthesis[i * 2]) +
+          evaluate(expression.substring(
+                  paranthesis[i * 2] + 1, paranthesis[i * 2 + 1]))
+              .toString() +
+          expression.substring(paranthesis[i * 2 + 1] + 1);
+    }
+  }
+
+  //If we have reached here there are no brackets and we can just multiplie and add everything
+
   //Remove all double negation
   expression = expression.replaceAll("!!", "");
-
-  //Depth=0 which means there are no brackets such as 0+1*0
+  //Handles negation
   for (int i = 0; i < expression.length - 1; i++) {
     if (expression[i] == "!" &&
         (expression[i + 1] == "0" || expression[i + 1] == "1")) {
@@ -46,8 +87,4 @@ int evaluate(String expression) {
     return 1;
   else //Something has fucked up if it has reached this statement, ups
     return -1;
-}
-
-main() {
-  // print(evaluate("!0"));
 }

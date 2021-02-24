27a8dc854692a78c6332061ec72f8f8b0e111d3d
Fran Marić (komp-VK) <fm.develoloper@gmail.com>
2021-02-24 01:51:45 +0100

    Changed from Boolean to Expression

---
+++ lib/widgets/InputExpression.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+//Functions
+import 'package:diglog/functions/expressionToStates.dart';
+import 'package:diglog/functions/varNamesFromExpression.dart';
+
+class InputBoolean extends StatelessWidget {
+  final Function changeState;
+  final Function submitState;
+  final String valid;
+
+  InputBoolean({this.changeState, this.submitState, this.valid});
+
+  void handleChange(String expression) {
+    changeState(expressionToStates(expression),
+        varNamesFromExpression(expression), expression);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      onChanged: handleChange,
+      onSubmitted: submitState,
+      cursorColor: Colors.blue[400],
+      decoration: InputDecoration(
+        hintText: "A + B*C",
+        errorText: valid,
+        fillColor: Colors.white,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5.0),
+          borderSide: BorderSide(color: Colors.grey[300]),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.blue[400]),
+        ),
+      ),
+    );
+  }
+}

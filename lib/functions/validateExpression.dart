c54bc86ead1208c864af0b630cf64dd04a6f6d4e
Fran Marić (komp-VK) <fm.develoloper@gmail.com>
2021-02-27 17:53:09 +0100

    Fixed validation

--- lib/functions/validateExpression.dart
+++ lib/functions/validateExpression.dart
@@ -1,3 +1,5 @@
+import 'package:diglog/functions/varNamesFromExpression.dart';
+
 const List<String> operators = [
   // literal
   "NOT",
@@ -31,6 +33,13 @@ String validateExpression(String expression) {
   if (expression == null || expression.isEmpty) return null;
   int depth = 0;
   for (int i = 0; i < expression.length; i++) {
+    if (!(operators +
+            "QWERTZUIOPŠĐŠŽASDFGHJKLČĆYXCVBNMqwertzuiopšđžasdfghjklčćyxycvbnm "
+                .split(""))
+        .contains(expression[i])) {
+      return "Variable names can contain only letters and numbers.";
+    }
+
     if (expression[i] == "(")
       depth++;
     else if (expression[i] == ")") depth--;
@@ -41,24 +50,6 @@ String validateExpression(String expression) {
 
   for (int i = 0; i < operators.length; i++) {
     expression = expression.replaceAll(operators[i], ";");
-  }
-
-  int start = 0;
-  String varName;
-
-  for (int i = 0; i < expression.length; i++) {
-    if (expression[i] == ";" || i == expression.length - 1) {
-      if (i == expression.length - 1 && expression[i] != ";")
-        varName = expression.substring(start, i + 1);
-      else {
-        varName = expression.substring(start, i);
-      }
-
-      if (!RegExp(r'^[a-zA-Z0-9]+$').hasMatch(varName))
-        return "Variable names can contain only letters and numbers.";
-
-      start = i + 1;
-    }
   }
 
   return null;

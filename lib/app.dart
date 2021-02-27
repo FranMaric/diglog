d726e3531a00907324471d3c94d67881c0e3fa4f
Fran Marić (komp-VK) <fm.develoloper@gmail.com>
2021-02-27 18:14:46 +0100

    Added SingleChildScrollView

--- lib/app.dart
+++ lib/app.dart
@@ -9,36 +9,38 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Text(
-                "Function minimizer",
-                style: TextStyle(
-                  fontSize: 25,
-                  fontFamily: "Roboto",
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Text(
+                  "Function minimizer",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontFamily: "Roboto",
+                  ),
                 ),
               ),
             ),
-          ),
-          Container(
-            height: 1,
-            color: Colors.black,
-          ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: InputPane(),
-              ),
-              Expanded(
-                child: OutputPane(),
-              ),
-            ],
-          ),
-        ],
+            Container(
+              height: 1,
+              color: Colors.black,
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: InputPane(),
+                ),
+                Expanded(
+                  child: OutputPane(),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }

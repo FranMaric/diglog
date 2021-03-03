7d4debfeb88c1bfb08560a401bc05d0eef722087
Fran Maric (laptop) <fm.develoloper@gmail.com>
2021-03-03 20:39:19 +0100

    Varsion 2.6.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -10,9 +10,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "a831aed7632958198e1eb1a874ed5e6c",
-"/": "a831aed7632958198e1eb1a874ed5e6c",
-"main.dart.js": "78c4bfc2d07fce3cd6306414a607c408",
+"index.html": "d4c834e9474d33f78e95706caceb8b81",
+"/": "d4c834e9474d33f78e95706caceb8b81",
+"main.dart.js": "78e4d741303427c8a366a67f6c662286",
 "manifest.json": "3780ec803ec615cb3ac8b1428b3e49f4",
 "version.json": "6343baa71566f5d3a5479cdb1a1523ff"
 };

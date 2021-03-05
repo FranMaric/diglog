2a203ff0385e96a475f40045dd85f6d7a8d9397a
Fran Marić (komp-VK) <fm.develoloper@gmail.com>
2021-03-05 23:41:32 +0100

    Removed # from url

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 import 'package:diglog/app.dart';
 
 //Provider
 import 'package:provider/provider.dart';
 import 'package:diglog/appState.dart';
 
-void main() => runApp(
-      ChangeNotifierProvider(
-        create: (context) => AppState(),
-        child: MaterialApp(
-          title: 'DigLog',
-          debugShowCheckedModeBanner: false,
-          home: App(),
-        ),
+void main() {
+  setPathUrlStrategy();
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => AppState(),
+      child: MaterialApp(
+        title: 'DigLog',
+        debugShowCheckedModeBanner: false,
+        home: App(),
       ),
-    );
+    ),
+  );
+}

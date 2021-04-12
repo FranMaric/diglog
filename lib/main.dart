634428de4811857199b35435cc058a22edc00844
Fran Marić <fran.maric.vk@gmail.com>
2021-04-12 10:44:51 +0200

    Removed # from url

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:diglog/app.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 //Provider
 import 'package:provider/provider.dart';
 import 'package:diglog/services/appState.dart';
 
 void main() {
+  setPathUrlStrategy();
   runApp(
     ChangeNotifierProvider(
       create: (context) => AppState(),

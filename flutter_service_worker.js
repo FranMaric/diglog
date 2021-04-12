ee7264f2e8cad27eb71997c84f78688961f95f60
Fran Marić <fran.maric.vk@gmail.com>
2021-04-12 11:56:06 +0200

    Version 2.6.5

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,13 +6,13 @@ const RESOURCES = {
   "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
-"assets/NOTICES": "f71bae1ba92691214fccfdd8b41897d2",
+"assets/NOTICES": "73bb5007be25f53685e4db3154905ac0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "ae1b39836fe82a5f39e0c08b16c69780",
-"/": "ae1b39836fe82a5f39e0c08b16c69780",
-"main.dart.js": "5a20f743e812c0ea2dc3bd373476fff4",
+"index.html": "206bbf6a3804a9906107aeaf00704e03",
+"/": "206bbf6a3804a9906107aeaf00704e03",
+"main.dart.js": "ba565b960117a930c851fcbfe7e39b95",
 "manifest.json": "3780ec803ec615cb3ac8b1428b3e49f4",
 "version.json": "6343baa71566f5d3a5479cdb1a1523ff"
 };
